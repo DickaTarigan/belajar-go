f6224d07c6484b66ba2bdbe228fba5f1b5b73411
DickaTarigan <dickaoctasius@gmail.com>
2026-08-18 01:04:50 +0700

    Menyelesaikan kalkulator

--- 01-basics/kalkulator/README.md
+++ 01-basics/kalkulator/README.md
@@ -1,10 +1,12 @@
 # Kalkulator (dengan error handling)
 
-## Apa yang gue pelajari di sini
-
+## Apa yang di pelajari di sini
+- Belajar membuat function
+- Belajar menggunakan error handling
+- nil artinya tidak error
 
 ## Kendala / bagian yang masih bingung
-
+- Pengaplikasian error ke codingan asli
 
 ## Link referensi (kalau ada)
 
--- 01-basics/kalkulator/main.go
+++ 01-basics/kalkulator/main.go
@@ -1,7 +1,38 @@
 package main
 
 import "fmt"
+import "errors"
+
+func tambah(x, y int) int {
+	return x + y
+}
+
+func kurang(x, y int) int {
+	return x - y
+}
+
+func kali(x, y int) int {
+	return x * y
+}
+
+func bagi(x, y int) (int, error) {
+	if y == 0 {
+		return 0, errors.New("tidak bisa di bagi 0")
+	}
+	return x / y, nil
+}
 
 func main() {
-	fmt.Println("TODO: Kalkulator (dengan error handling)")
+	x := 3
+	y := 4
+	fmt.Println(tambah(x, y))
+	fmt.Println(kurang(x, y))
+	fmt.Println(kali(x, y))
+
+	hasil, err := bagi(x, y)
+	if err != nil {
+		fmt.Println("Error :", err)
+		return
+	}
+	fmt.Println(hasil)
 }
