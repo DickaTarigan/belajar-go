98afc9e22dba21e2c695bc8d3b649f3a23b55f4e
DickaTarigan <dickaoctasius@gmail.com>
2026-08-19 02:35:32 +0700

    Menyelesaikan slice-manipulation: Reverse, Minmax, dan Hapus Duplikat

--- 02-slice-map/slice-manipulation/README.md
+++ 02-slice-map/slice-manipulation/README.md
@@ -1,10 +1,12 @@
 # Slice Manipulation (reverse, max/min, dedupe)
 
-## Apa yang gue pelajari di sini
-
+## Apa yang di pelajari di sini
+- Menggunakan Apend di slice
+- Menyusun logika untuk reverse, minmax, dan hapus duplikat
 
 ## Kendala / bagian yang masih bingung
-
+- Menyusun logika nya dan mengaplikasinnya menjadi codingan
+- Masih terbata bata dalam menulis sintax nya
 
 ## Link referensi (kalau ada)
 
--- 02-slice-map/slice-manipulation/main.go
+++ 02-slice-map/slice-manipulation/main.go
@@ -2,6 +2,50 @@ package main
 
 import "fmt"
 
+func reverse(angka []int) []int {
+	hasil := make([]int, 0, len(angka))
+	for i := len(angka) - 1; i >= 0; i-- {
+		hasil = append(hasil, angka[i])
+	}
+	return hasil
+}
+
+func minmax(angka []int) (int, int) {
+	min := angka[0]
+	max := angka[0]
+
+	for _, val := range angka {
+		if val < min {
+			min = val
+		}
+		if val > max {
+			max = val
+		}
+	}
+	return min, max
+}
+
+func hapusDuplikat(angka []int) []int {
+	penampungan := map[int]bool{}
+	var slice []int
+
+	for _, val := range angka {
+		if !penampungan[val] {
+			slice = append(slice, val)
+			penampungan[val] = true
+		}
+	}
+	return slice
+}
+
 func main() {
-	fmt.Println("TODO: Slice Manipulation (reverse, max/min, dedupe)")
+	angka := []int{1, 3, 5, 7, 5, 3, 632, 4}
+
+	fmt.Println("reverse:", reverse(angka))
+
+	nilaiMin, nilaiMax := minmax(angka)
+	fmt.Printf("min: %d, max: %d \n", nilaiMin, nilaiMax)
+
+	fmt.Println("Hapus duplikat:", hapusDuplikat(angka))
+
 }
