77b8bcd972e771beaa1ad4d180f6fd52b2f1a3f4
DickaTarigan <dickaoctasius@gmail.com>
2026-08-21 01:34:25 +0700

    Menyelesaikan inventori: tambah, cari, dan hapus barang

--- 02-slice-map/inventory/README.md
+++ 02-slice-map/inventory/README.md
@@ -1,10 +1,12 @@
 # Simple Inventory (slice of struct)
 
-## Apa yang gue pelajari di sini
-
+## Apa yang di pelajari di sini
+- Belajar menambah, mencari, dan menghapus slice
+- struct bisa di slice
 
 ## Kendala / bagian yang masih bingung
-
+- Abstraksi algoritma nya
+- masih sering miss kalkulasi, blum efisien
 
 ## Link referensi (kalau ada)
 
--- 02-slice-map/inventory/main.go
+++ 02-slice-map/inventory/main.go
@@ -2,6 +2,64 @@ package main
 
 import "fmt"
 
+type Item struct {
+	Nama  string
+	Harga int
+}
+
+func tambah(inv []Item, nama string, harga int) []Item {
+	itemBaru := Item{
+		Nama:  nama,
+		Harga: harga,
+	}
+
+	inv = append(inv, itemBaru)
+
+	return inv
+
+}
+
+func cari(inv []Item, nama string) bool {
+	itemCari := Item{
+		Nama: nama,
+	}
+
+	for _, item := range inv {
+		if item.Nama == itemCari.Nama {
+			return true
+		}
+	}
+
+	return false
+}
+
+func hapus(inv []Item, nama string) []Item {
+	var keranjang []Item
+
+	itemHapus := Item{
+		Nama: nama,
+	}
+
+	for _, item := range inv {
+		if item.Nama != itemHapus.Nama {
+			keranjang = append(keranjang, item)
+		}
+	}
+	return keranjang
+}
+
 func main() {
-	fmt.Println("TODO: Simple Inventory (slice of struct)")
+	var inventory []Item
+
+	inventory = tambah(inventory, "Laptop", 15000)
+	inventory = tambah(inventory, "Smartphone", 2000)
+
+	fmt.Println(inventory)
+
+	fmt.Println(cari(inventory, "Meki"))
+	fmt.Println(cari(inventory, "Laptop"))
+
+	fmt.Println(hapus(inventory, "Ayam"))
+	fmt.Println(hapus(inventory, "Laptop"))
+
 }
