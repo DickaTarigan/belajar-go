3cde93862d5db70b5535b7c73fe0a2cbf92fb37e
DickaTarigan <dickaoctasius@gmail.com>
2026-08-21 23:27:15 +0700

    Menyelesaikan seluruh exercie  struct-interface

--- 03-struct-interface/bentuk/README.md
+++ 03-struct-interface/bentuk/README.md
@@ -1,10 +1,11 @@
 # Bentuk (interface Luas())
 
-## Apa yang gue pelajari di sini
-
+## Apa yang di pelajari di sini
+- Penyempurnaan konsep Struct, Interface, dan Method
+- Polymorphism dan Open/Closed principle
 
 ## Kendala / bagian yang masih bingung
-
+-
 
 ## Link referensi (kalau ada)
 
--- 03-struct-interface/bentuk/main.go
+++ 03-struct-interface/bentuk/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+type BangunDatar interface {
+	Luas() float64
+}
+
+type Lingkaran struct {
+	r float64
+}
+
+func (l Lingkaran) Luas() float64 {
+	return math.Pi * l.r * l.r
+}
+
+type Persegi struct {
+	s float64
+}
+
+func (p Persegi) Luas() float64 {
+	return p.s * p.s
+}
+
+type Segitiga struct {
+	a float64
+	t float64
+}
+
+func (s Segitiga) Luas() float64 {
+	return s.a * s.t / 2
+}
+
+func HitungLuas(b BangunDatar) float64 {
+	return b.Luas()
+}
 
 func main() {
-	fmt.Println("TODO: Bentuk (interface Luas())")
+	lingkaran := Lingkaran{r: 10}
+	persegi := Persegi{s: 4}
+	segitiga := Segitiga{
+		a: 5,
+		t: 6,
+	}
+
+	fmt.Println("Luas lingkaran:", HitungLuas(lingkaran))
+	fmt.Println("Luas persegi:", HitungLuas(persegi))
+	fmt.Println("Luas segitiga:", HitungLuas(segitiga))
 }
