18f9c660363bdc44203d43f4b22c9fb650bc3317
DickaTarigan <dickaoctasius@gmail.com>
2026-08-21 22:31:40 +0700

    Menyelesaikan hewan: Interface, Struct, dan Method

--- 03-struct-interface/hewan/README.md
+++ 03-struct-interface/hewan/README.md
@@ -1,10 +1,11 @@
 # Hewan (struct + method + interface)
 
-## Apa yang gue pelajari di sini
-
+## Apa yang di pelajari di sini
+- Belajar Konsep Interface, Struct, dan Method
+- Perbedaan Pointer Receiver dan Value Receiver
 
 ## Kendala / bagian yang masih bingung
-
+- Memahami 80 persen, karena rasanya ada beberapa yang blum klik
 
 ## Link referensi (kalau ada)
 
--- 03-struct-interface/hewan/main.go
+++ 03-struct-interface/hewan/main.go
@@ -2,6 +2,45 @@ package main
 
 import "fmt"
 
+type Hewan interface {
+	Bersuara() string
+	Bernama() string
+}
+type Kucing struct {
+	Nama string
+}
+
+func (k *Kucing) Bersuara() string {
+	return "MIAWWW"
+}
+func (k *Kucing) Bernama() string {
+	return k.Nama
+}
+
+type Anjing struct {
+	Nama string
+}
+
+func (a *Anjing) Bersuara() string {
+	return "Gong Gong"
+}
+func (a *Anjing) Bernama() string {
+	return a.Nama
+}
+
+func CetakSuara(h Hewan) {
+	fmt.Printf("Nama Hewan: %s, Suara: %s\n", h.Bernama(), h.Bersuara())
+}
+
 func main() {
-	fmt.Println("TODO: Hewan (struct + method + interface)")
+	kucing := Kucing{
+		Nama: "Asko",
+	}
+
+	anjing := Anjing{
+		Nama: "Black",
+	}
+
+	CetakSuara(&kucing)
+	CetakSuara(&anjing)
 }
