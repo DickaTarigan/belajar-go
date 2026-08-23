f6fd727db96d328801b7d607ba36cf9b439b24a9
DickaTarigan <dickaoctasius@gmail.com>
2026-08-24 00:48:44 +0700

    Menyelesaikan todo-json: json.Marshal, os.ReadFile, dan os.WriteFile

--- 04-error-file/todo-json/README.md
+++ 04-error-file/todo-json/README.md
@@ -1,10 +1,11 @@
 # Todo App (baca/tulis JSON file)
 
-## Apa yang gue pelajari di sini
-
+## Apa yang di pelajari di sini
+- Belajar mengubah struct menjadi json dan sebaliknya
+- Menggunakan json.Marshal, json Unmarshal, os.WriteFile, dan os.ReadFile
 
 ## Kendala / bagian yang masih bingung
-
+- Belum terbiasa
 
 ## Link referensi (kalau ada)
 
--- 04-error-file/todo-json/main.go
+++ 04-error-file/todo-json/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Todo struct {
+	ID      int
+	Judul   string
+	Selesai bool
+}
+
+func save(namaFile string, data []Todo) error {
+	jsonData, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return fmt.Errorf("gagal convert ke json: %w", err)
+	}
+
+	err = os.WriteFile(namaFile, jsonData, 0664)
+	if err != nil {
+		return fmt.Errorf("gagal menulis file: %w", err)
+	}
+	return nil
+}
+
+func load(namaFile string) ([]Todo, error) {
+	Hasil := []Todo{}
+	jsonData, err := os.ReadFile(namaFile)
+	if err != nil {
+		return nil, fmt.Errorf("File gagal di load: %w", err)
+	}
+
+	err = json.Unmarshal(jsonData, &Hasil)
+	if err != nil {
+		return nil, fmt.Errorf("gagal mengubah menjadi struct: %w", err)
+	}
+
+	return Hasil, nil
+}
 
 func main() {
-	fmt.Println("TODO: Todo App (baca/tulis JSON file)")
+	daftarTodo := []Todo{
+		{ID: 1, Judul: "Belajar error go", Selesai: true},
+		{ID: 2, Judul: "Belajar convert json", Selesai: true},
+	}
+
+	err := save("Belajar convert.json", daftarTodo)
+	if err != nil {
+		fmt.Println("Gagal menyimpan:", err)
+		return
+	}
+	fmt.Println("Data berhasil disimpan")
+
+	jsonToStruct, err := load("Belajar convert.json")
+	if err != nil {
+		fmt.Println("Gagal convert ke struct:", err)
+		return
+	}
+	fmt.Println(jsonToStruct)
 }
