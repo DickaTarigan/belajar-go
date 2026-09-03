1ad5a973cc1450b4afcf9f8767d5431abbb4a7f7
DickaTarigan <dickaoctasius@gmail.com>
2026-09-03 16:36:36 +0700

    Menyelesaikan channel-basic: paham buffered dan unbuffered

--- 05-concurrency/channel-basic/README.md
+++ 05-concurrency/channel-basic/README.md
@@ -1,10 +1,11 @@
 # Channel Dasar
 
-## Apa yang gue pelajari di sini
-
+## Apa yang di pelajari di sini
+- Belajar buffer chanel dan unbuffer chanel
+- Alur eksekusi buffer dan unbuffer chanel
 
 ## Kendala / bagian yang masih bingung
-
+- butuh adaptasi dan lumayan lama memahaminya
 
 ## Link referensi (kalau ada)
 
--- 05-concurrency/channel-basic/main.go
+++ 05-concurrency/channel-basic/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("TODO: Channel Dasar")
+	ch := make(chan string, 2) // buffer 2 slot
+
+	go func() {
+		fmt.Println("Goroutine: mau kirim pesan 1...")
+		ch <- "pesan 1"
+		fmt.Println("Goroutine: pesan 1 terkirim!") // ini bisa LANGSUNG muncul, gak nunggu penerima
+		fmt.Println("Goroutine: mau kirim pesan 2...")
+		ch <- "pesan 2"
+		fmt.Println("Goroutine: pesan 2 terkirim!")
+	}()
+
+	fmt.Println("Main: baru sekarang mau nerima...")
+	fmt.Println("Main terima:", <-ch)
+	fmt.Println("Main terima:", <-ch)
 }
